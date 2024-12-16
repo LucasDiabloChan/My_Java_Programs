38d63446c583f9734c4d40b7aa02738c555a7b96
AsherLucRen <82063404+LucasDiabloChan@users.noreply.github.com>
2024-12-16 17:01:26 -0300

    Desenvolvimento do exercicio da aula 100

--- CursosNelioAlves/JavaCompleto/Exercicios/ExercicioAula100.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-
-public class ExercicioAula100{
-  public static void main(String[] args){
-    System.out.println("To continue...");
-  }
-}
---
+++ CursosNelioAlves/JavaCompleto/Exercicios/ExercicioAula100/Main.java
@@ -0,0 +1,43 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main{
+    private Scanner sc = null;
+
+    public static void main(){
+        List<Funcionario> listaFuncionarios;
+        int qtdFuncionarios = 0;
+
+        System.out.println("Digite a quantidade de funcionários a serem cadastrados: ");
+        qtdFuncionarios = sc.nextInt();
+        
+        listaFuncionarios = coletarDadosFuncionarios(qtdFuncionarios);
+        listarDadosFuncionarios(listaFuncionarios);
+    }
+
+    public static List<Funcionario> coletarDadosFuncionarios(int qtdFuncionarios){
+        List<Funcionario> listaFuncionarios = null;
+        sc = new Scanner(System.in);
+
+        for(int i = 1; i <= qtdFuncionarios; i++){
+            int idFunc = 0;
+            String nomeFunc = null;
+            double salarioFunc = 0.0;
+
+            System.out.println("# "+ i +"º Funcionário(a) ");
+            System.out.println("Id: ");
+            idFunc = sc.nextInt();
+
+            System.out.println("Nome: ");
+            nomeFunc = sc.nextString();
+
+            System.out.println("Salário: ");
+            salarioFunc = sc.nextDouble();
+
+            listaFuncionarios.add(new Funcionario(idFunc, nomeFunc, salarioFunc));
+        }
+
+        return listaFuncionarios;
+    }
+}
